e24d1a79942a5132237bd1d639f31ca467360935
AlanRecalde <114194256+AlanRecalde@users.noreply.github.com>
2022-09-22 20:28:18 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+ public class main   {
+
+        public static void main(String[] args) {
+            int resultado;
+            resultado = suma(10,10,10);
+
+            System.out.println(resultado);
+        }
+
+
+            public static int suma(int a, int b, int c) {
+                return a + b + c;
+            }
+        }
